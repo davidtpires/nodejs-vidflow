d60d4ebd98ecc5b9142e651b8bb8a4a8c4a41917
David T. <davidt.info@gmail.com>
2025-02-28 14:42:44 -0300

    Usando NPM para instalar o Axios

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-console.log(axios);
 
 const containerVideos = document.querySelector(".videos__container");
 
 async function buscarEMostrarVideos() {
   try {
     const busca = await axios.get("http://localhost:3000/videos");
-    console.log(busca)
+
     const videos = busca.data;
 
     videos.forEach((video) => {
