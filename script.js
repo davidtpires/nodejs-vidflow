ab0bd0d40966255831b5c24f1cc9f7256ec04a11
David T. <davidt.info@gmail.com>
2025-02-28 14:37:24 -0300

    Usando CDN do Axios

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+console.log(axios);
+
 const containerVideos = document.querySelector(".videos__container");
 
 async function buscarEMostrarVideos() {
   try {
-    const busca = await fetch("http://localhost:3000/videos");
-    const videos = await busca.json();
+    const busca = await axios.get("http://localhost:3000/videos");
+    console.log(busca)
+    const videos = busca.data;
 
     videos.forEach((video) => {
       if (video.categoria == "") {
